f046f31309f6ee365541f3cb2523251fbb76e549
HarshavardhanVemali <vemalivardhan@gmail.com>
2024-05-19 15:33:05 +0530

    test

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1e&=k-rv#43b(ic&(6fd&5!s+*368ng$0ufkoz)r)bd1e6_83h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG','True')=="True"
 
 ALLOWED_HOSTS = ["*"]
 
